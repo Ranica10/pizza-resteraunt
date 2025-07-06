2a4101437e3466daf0d0378245ef52e63c920557
Ranica10 <ranicachawla101@gmail.com>
2025-07-06 00:00:18 -0400

    create intro section

--- pizza-resteraunt/src/App.vue
+++ pizza-resteraunt/src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
 
+import Intro from './components/sections/Intro.vue';
+
 </script>
 
 <template>
-    <Navbar></Navbar>
+    <div class="app">
+        <Navbar></Navbar>
+        <div class="nav-spacer" style="height: 72px;"></div>
+        <Intro></Intro>
+    </div>
 </template>
